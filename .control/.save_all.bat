0448cc86fc2d521f2b84db4634eb64eabc81831c
Bear Riley <46008563+BehrRiley@users.noreply.github.com>
2020-03-09 02:45:14 -0400

    Update .save_all.bat

--- .control/.save_all.bat
+++ .control/.save_all.bat
@@ -1,41 +1,43 @@
-:begin
+
 @echo off
 
-REM --xcopy--
-REM xcopy "from" "to/backup"
-REM /W Prompts you to press a key before copying.
-REM /E Copies directories and subdirectories, including empty ones.
-REM /H Copies hidden and system files
-REM /R Overwrites read-only files
-REM /Y Suppresses prompting to confirm you want to overwrite an existing destination file.
-REM Note:
-REM /B Copies the Symbolic Link itself versus the target of the link.
+echo ---------------------------------------------------------------------------------------
+echo ------------ Starting Backup System ---------------------------------------------------
+echo ---------------------------------------------------------------------------------------
 
-REM --dir--
+timeout 43200 /nobreak
+:begin
+
+REM ------------- Directories --------------------------------------------------------------
 REM 1.15.2 Production Servers    -  ..\Servers\1.15.2 Production
 REM 1.15.2 Construction Servers  -  ..\Servers\1.15.2 Construction
 REM 1.15.2 Construction Servers  -  ..\Servers\1.12.2 Construction
 REM Data Servers                 -  ..\Servers\0.0.0 Data
-REM Backups                      -  ..\.backups\*time
+REM Backups                      -  ..\.backups\##h\
 
-REM - 1.15.2 Production Servers
-rd /s /q "..\.backups\1.15.2 Production\96h"
-move /Y “..\.backups\1.15.2 Production\84_backup” “..\.backups\1.15.2 Production\96h_backup”
-move /Y “..\.backups\1.15.2 Production\72_backup” “..\.backups\1.15.2 Production\84h_backup”
-move /Y “..\.backups\1.15.2 Production\60_backup” “..\.backups\1.15.2 Production\72h_backup”
-move /Y “..\.backups\1.15.2 Production\48_backup” “..\.backups\1.15.2 Production\60h_backup”
-move /Y “..\.backups\1.15.2 Production\36_backup” “..\.backups\1.15.2 Production\48h_backup”
-move /Y “..\.backups\1.15.2 Production\24_backup” “..\.backups\1.15.2 Production\36h_backup”
-move /Y “..\.backups\1.15.2 Production\12_backup” “..\.backups\1.15.2 Production\24h_backup”
-xcopy /W /E /H /R /Y “..\Servers\1.15.2 Production\” “..\.backups\1.15.2 Production\12h_backup”
+REM ------------- 1.15.2 Production --------------------------------------------------------
+rd /s /q "..\.backups\120h\1.15.2 Production Backup"
+move /Y “..\.backups\108h\1.15.2 Production Backup” “..\.backups\120h\”
+move /Y “..\.backups\96h\1.15.2 Production Backup” “..\.backups\108h\”
+move /Y “..\.backups\84h\1.15.2 Production Backup” “..\.backups\96h\”
+move /Y “..\.backups\72h\1.15.2 Production Backup” “..\.backups\84h\”
+move /Y “..\.backups\60h\1.15.2 Production Backup” “..\.backups\72h\”
+move /Y “..\.backups\48h\1.15.2 Production Backup” “..\.backups\60h\”
+move /Y “..\.backups\36h\1.15.2 Production Backup” “..\.backups\48h\”
+move /Y “..\.backups\24h\1.15.2 Production Backup” “..\.backups\36h\”
+move /Y “..\.backups\12h\” “..\.backups\24h\”
+xcopy /E /H /R /Y “..\Servers\1.15.2 Production” “..\.backups\12h\1.15.2 Production Backup\”
+
+REM ------------- Other Placeholder --------------------------------------------------------
 
 for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
 set mytime=%time%
-echo --------------------------------------------------
-echo ----- Completion: %mydate%    %mytime% ------
-echo --------------------------------------------------
-echo --------------------------------------------------
-echo ----- Loop Complete ------------------------------
-echo --------------------------------------------------
+echo ---------------------------------------------------------------------------------------
+echo ------------ Completion: %mydate%    %mytime% ------------------------------------
+echo ---------------------------------------------------------------------------------------
+echo ---------------------------------------------------------------------------------------
+echo ------------ Loop Complete ------------------------------------------------------------
+echo ---------------------------------------------------------------------------------------
+
 timeout 43200 /nobreak
 goto begin
