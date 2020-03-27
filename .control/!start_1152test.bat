545bd494a5682c455d46e5e141dc0a85de222d89
Bear Riley <46008563+BehrRiley@users.noreply.github.com>
2020-03-27 19:41:10 -0400

    Update !start_1152test.bat
    Added new features:
    - Prompts for PAPER or SPIGOT server
    - Prompts for Test Server ID
    - Creates Test Server ID from template if nonexistant
    - Updates jar file with duplicate inserted after restart
    - Checks if jar exists before java loop

--- .control/!start_1152test.bat
+++ .control/!start_1152test.bat
@@ -1,8 +1,42 @@
-:begin
-cd "..\Servers\1.15.2 Test"
+@Echo Off
+set /p ID="Type Test Server ID:"
+cls
+if EXIST "..\Servers\1.15.2 Test%ID%" goto start
 
-java -Xms1G -Xmx1G -jar paper-145.jar -nogui
+:CreateDir
+echo [91mServer 1.15.2 Test%ID% does not exist. Creating from template.[0m
+xcopy /E /H /R /Y "..\Servers\Templates\1.15.2 Template" "..\Servers\"
+ren "..\Servers\1.15.2 Template" “1.15.2 Test%ID%"
 
-timeout 3
-echo resuming server...
-goto begin
+:start
+cd "..\Servers\1.15.2 Test%ID%"
+
+:VersionCheck
+set /P Version="Run Spigot or Paper?:"
+:Restart
+if %Version%==Spigot goto SpigotStart
+if %Version%==Paper goto PaperStart
+goto BadVar
+
+:PaperStart
+if exist "..\..\.jars\Version\1.15.2 Paper.jar" (
+    java -Xms1G -Xmx1G -jar "..\..\.jars\Version\1.15.2 Paper.jar" -nogui
+    ) else (
+    goto JarNotFound
+)
+timeout 5
+goto Restart
+
+:SpigotStart
+if exist "..\..\.jars\Version\1.15.2 Spigot.jar" (
+    java -Xms1G -Xmx1G -jar "..\..\.jars\Version\1.15.2 Spigot.jar" -nogui
+    ) else (
+    goto JarNotFound
+)
+timeout 5
+goto Restart
+
+:JarNotFound
+echo [91m%Version% jar file not found.[0m
+timeout 30
+goto restart
