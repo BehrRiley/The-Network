188cc8466b605f21d0c0d5add22cd44f677a5c5f
Bear Riley <46008563+BehrRiley@users.noreply.github.com>
2020-03-27 19:40:35 -0400

    Update !start_1152gateway.bat
    Added new features:
    - Prompts for BUNGEECORD or WATERFALL proxy
    - Updates jar file with duplicate inserted after restart
    - Checks if jar exists before java loop

--- .control/!start_1152gateway.bat
+++ .control/!start_1152gateway.bat
@@ -1,8 +1,55 @@
-:begin
+@Echo Off
 cd "..\Servers\1.15.2 Gateway"
+cls
+:VersionCheck
+set /P Version="Run Bungeecord or Waterfall?:"
+:Restart
+if %Version%==Bungeecord goto BungeecordCheck
+if %Version%==Waterfall goto WaterfallCheck
+goto BadVar
 
-java -Xms1G -Xmx1G -jar waterfall.jar -nogui
+:BungeecordCheck
+if exist "..\..\.jars\Version\Waterfall (2).jar" (
+    echo [92mUpdate found - updating.[0m
+    timeout 1 
+    del "..\..\.jars\Version\Waterfall.jar"
+    ren "..\..\.jars\Version\Waterfall (2).jar" "Waterfall.jar"
+    )
+goto %Version%Start
 
-timeout 3
-echo resuming server...
-goto begin
+:WaterfallCheck
+if exist "..\..\.jars\Version\Waterfall (2).jar" (
+    echo [92mUpdate found - updating.[0m
+    del "..\..\.jars\Version\Waterfall.jar"
+    ren "..\..\.jars\Version\Waterfall (2).jar" "Waterfall.jar"
+    )
+goto %Version%Start
+
+:BadVar
+echo [93m%Version%[0m [91mis an invalid Version.[0m
+echo [91mPlease type one of the valid versions:[0m
+echo [91m[ Waterfall ] or [ Bungeecord ][0m
+goto VersionCheck
+
+:WaterfallStart
+if exist "..\..\.jars\Version\Waterfall.jar" (
+    java -Xms1G -Xmx1G -jar "..\..\.jars\Version\Waterfall.jar"
+    ) else (
+    goto JarNotFound
+)
+timeout 5
+goto Restart
+
+:BungeecordStart
+if exist "..\..\.jars\Version\Bungeecord.jar" (
+    java -Xms1G -Xmx1G -jar "..\..\.jars\Version\Bungeecord.jar"
+    ) else (
+    goto JarNotFound
+)
+timeout 5
+goto Restart
+
+:JarNotFound
+echo [91m%Version% jar file not found.[0m
+timeout 30
+goto restart
